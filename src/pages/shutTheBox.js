7d0a9c0f008c0c08f0d6a4c08074eb6f238f3bef
Dan Dzoan <dandzoan@gmail.com>
2020-04-25 14:13:08 -0700

    remove some gatsby styles, need to copy game files to deploy

--- src/pages/shutTheBox.js
+++ src/pages/shutTheBox.js
@@ -1,14 +1,13 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ShutTheBox from "../components/shutTheBox";
+import ShutTheBox from "../components/game/game";
 
 const ShutTheBoxPage = () => (
-  <Layout>
+  <>
     <SEO title="Shut The Box" />
     <ShutTheBox />
-  </Layout>
+  </>
 );
 
 export default ShutTheBoxPage
